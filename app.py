1fa1eca25330db791dac4eece618ff51bf2cfc36
Your Name <you@example.com>
2023-06-08 18:01:45 +0530

    second commit

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
 @app.route('/store', methods=['GET'])
 def get_stores():
    return {"stores": stores}
+
+if __name__ == '__main__':
+    app.run(debug=false, host='0.0.0.0')
