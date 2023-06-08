813e7b823095c29f5273890aeb3d550965b7ba46
Your Name <you@example.com>
2023-06-08 20:19:18 +0530

    yolov4 added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import urllib.request
 
 app = Flask(__name__)
 
@@ -6,7 +7,9 @@ stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
 
 @app.route('/store', methods=['GET'])
 def get_stores():
-   return {"stores": stores}
+    urllib.request.urlretrieve('https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.cfg', 'yolov4.cfg')
+    urllib.request.urlretrieve('https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights', 'yolov4.weights')
+    return {"stores": stores}
 
 if __name__ == '__main__':
     app.run(debug=false, host='0.0.0.0')
