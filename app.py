79a53f38721820ef3a2a95242d68da631fc5db98
Your Name <you@example.com>
2023-06-08 17:53:41 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
+
+@app.route('/store', methods=['GET'])
+def get_stores():
+   return {"stores": stores}
